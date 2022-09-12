48eac24ee411e012176b9c71b3c8042eb7ad97ab
Jim Fellows <jim.fellows01@gmail.com>
2022-09-11 20:56:45 -0700

    basic line chart example
    Used https://www.youtube.com/watch?v=aw8d2f3xfPA
    and followed line chart sample and docs from:
    https://www.chartjs.org/docs/latest/charts/line.html#segment
    
    https://www.chartjs.org/docs/latest/samples/line/line.html

---
+++ vue/saunamonitor/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <LineChart msg="Sauna Monitor" />
+</template>
+
+<script>
+import LineChart from "./components/LineChart.vue";
+
+export default {
+  name: "App",
+  components: {
+    LineChart,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/saunamonitor/src/components/LineChart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+  <canvas id="myChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js/auto";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  mounted() {
+    console.log("component mounted");
+
+    const ctx = document.getElementById("myChart");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: [1, 2, 3, 4],
+        datasets: [
+          {
+            label: "Temperature (F)",
+            data: [77, 78, 80, 86],
+            // showLine: true,
+            borderColor: "red",
+          },
+          {
+            label: "Relative Humidity",
+            data: [66, 68, 69, 77],
+            borderColor: "blue",
+          },
+        ],
+      },
+    });
+
+    myChart;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
