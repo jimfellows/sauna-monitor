2aff95d087a0ff0925c3c4481137e2a3afd8abb4
Jim Fellows <jim.fellows01@gmail.com>
2022-10-30 19:37:51 -0700

    chartjs updated with dual axis, pulling data via pouch

--- vue/saunamonitor/src/components/LineChart.vue
+++ vue/saunamonitor/src/components/LineChart.vue
@@ -8,31 +8,76 @@
 <script>
 import Chart from "chart.js/auto";
 export default {
-  name: "HelloWorld",
+  name: "LineChart",
   props: {
     msg: String,
   },
   mounted() {
     console.log("component mounted");
+    // const PouchDB = require("pouchdb");
 
+    // const PouchDB = require("pouchdb-browser");
+    const PouchDB = require("pouchdb").default;
+    const sensorData = new PouchDB(
+      "http://admin:admin@192.168.1.101:5984/home-sensors"
+    );
+    const temps = [];
+    const hums = [];
+    const xaxis = [];
+    const getData = function () {
+      console.info("Getting data");
+      sensorData.query("basicQueries/todaysSaunaReadings").then(function (doc) {
+        for (const r of doc.rows) {
+          temps.push(r.value.tempF);
+          hums.push(r.value.humidity);
+          xaxis.push(r.value.dt);
+        }
+      });
+    };
+    getData();
+    Promise.resolve(temps);
+    Promise.resolve(hums);
+    Promise.resolve(xaxis);
+    console.info(hums);
+    // console.info(data);
     const ctx = document.getElementById("myChart");
     const myChart = new Chart(ctx, {
       type: "line",
       data: {
-        labels: [1, 2, 3, 4],
+        labels: xaxis,
         datasets: [
           {
             label: "Temperature (F)",
-            data: [77, 78, 80, 86],
-            // showLine: true,
+            data: temps,
+            yAxisId: "tempY",
             borderColor: "red",
           },
           {
             label: "Relative Humidity",
-            data: [66, 68, 69, 77],
+            data: hums,
+            yAxisId: "humY",
             borderColor: "blue",
           },
         ],
+      },
+      options: {
+        scales: {
+          y1: {
+            stacked: false,
+            position: "left",
+            type: "linear",
+            scaleLabel: {
+              display: true,
+            },
+            id: "tempY",
+          },
+          y2: {
+            stacked: false,
+            position: "right",
+            type: "linear",
+            id: "humY",
+          },
+        },
       },
     });
 
